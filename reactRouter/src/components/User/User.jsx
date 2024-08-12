8ef9e9f42392502fffd7d04c9b8eba7146b25512
sujan <budhasujan3@gmail.com>
2024-08-12 21:39:13 +0545

    reactrouter project

--- reactRouter/src/components/User/User.jsx
+++ reactRouter/src/components/User/User.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom"
 
 const User = () => {
+    // In React Router, the useParams hook is used to access the dynamic parameters from the URL in a component. This is particularly useful when you need to retrieve specific data based on the URL structure, such as user IDs, product IDs, or other dynamic segments in the URL.
     const {userid} = useParams();
   return (
     <div>
-        User: {userid}
+        User: {userid} 
     </div>
   )
 }
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -7,6 +7,7 @@ import About from './components/About Us/About.jsx'
 import Contact from './components/Contact Us/Contact.jsx'
 import Home from './components/Home/Home.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -36,6 +37,10 @@ const router = createBrowserRouter(
       <Route path='about' element={<About/>}/>
       <Route path='contact' element={<Contact/>}/>
       <Route path='user/:userid' element={<User/>}/>
+      <Route
+        loader={githubInfoLoader}
+       path='github'
+        element={<Github/>}/>
     </Route>
   )
 )
